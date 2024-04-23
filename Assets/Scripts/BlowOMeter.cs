e184e76d14e6921826cb6ee06656a3e96499f2f8
Otter <clamarped@gmail.com>
2024-04-23 18:04:19 +0200

    more microphone stuff

---
+++ Assets/Scripts/BlowOMeter.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlowOMeter : MonoBehaviour
+{
+    public Slider slider;
+    public AudioSource source;
+    public MicrophoneSensor microphoneScript;
+
+    public int countUp = 0;
+    public float minimumDecibel = 1;
+
+
+    void Start()
+    {
+        FillUp();
+    }
+
+
+    void Update()
+    {
+       
+    }
+
+    /// <summary>
+    /// https://www.w3schools.com/cs/cs_while_loop.php
+    /// </summary>
+    public void FillUp()
+    {
+        int i = 0;
+        while (i <= 100)
+        {
+            if (microphoneScript.GetDecibelFromMicrophone()>minimumDecibel) 
+            {
+                SetSlider(countUp++);
+                i++;
+            }
+        }
+    }
+
+    public void SetSlider(float tmp)
+    { slider.value = tmp; }
+}
