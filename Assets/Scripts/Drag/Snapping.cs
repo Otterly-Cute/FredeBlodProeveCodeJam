cab30cc28759ff0ea6f1bf89af7c0d2f2a61f6d7
døffer <christoffer.b.thomsen@gmail.com>
2024-04-24 13:53:04 +0200

    SNAPPER

--- Assets/Scripts/Drag/Snapping.cs
+++ Assets/Scripts/Drag/Snapping.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Snapping : Drag
 {
     private float movementTime = 15f;
-    private System.Nullable<Vector3>
+   
     // Update is called once per frame
     void Update()
     {
