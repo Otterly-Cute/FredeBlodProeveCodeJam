29ef70ac39485bb74ac7f730e2e0fd6b5410cdb8
tikzyy <145325630+tikzyy@users.noreply.github.com>
2024-04-25 11:02:56 +0200

    Update SceneLoader.cs

--- Assets/Scripts/SceneChangerStuff/SceneLoader.cs
+++ Assets/Scripts/SceneChangerStuff/SceneLoader.cs
@@ -19,14 +19,13 @@ public class SceneLoader : MonoBehaviour
     IEnumerator LoadAsynchronously(string sceneName)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        loadingScreen.SetActive(true); // vi kan bruge samme koncept her til en transitionary skærm
 
-        loadingScreen.SetActive(true);
+        // vi skal nok også sætte noget op som deaktiverer den, men det burde være ret lige til når det er
 
-        while (!operation.isDone)
-        {
+        while (!operation.isDone) {
             float progress = Mathf.Clamp01(operation.progress / .9f);
-            
-            slider.value = progress;
+            slider.value = progress; // behøves ikke medmindre vi vil have en progress bar
 
             yield return null;
         }
