294a88662744f7215a9f9ee0c7e2e521d83647b8
tikzyy <145325630+tikzyy@users.noreply.github.com>
2024-04-25 11:07:14 +0200

    gfdgsfg

--- Assets/Scripts/SceneChangerStuff/SceneLoader.cs
+++ Assets/Scripts/SceneChangerStuff/SceneLoader.cs
@@ -24,7 +24,9 @@ public class SceneLoader : MonoBehaviour
         // vi skal nok også sætte noget op som deaktiverer den, men det burde være ret lige til når det er
 
         while (!operation.isDone) {
-            float progress = Mathf.Clamp01(operation.progress / .9f);
+            float progress = Mathf.Clamp01(operation.progress / .9f); // .9 er fordi at unity loader kun
+            // til 90%, de sidste 10% er for at slette alt i forrig scene - de kan vi ikke vise.
+
             slider.value = progress; // behøves ikke medmindre vi vil have en progress bar
 
             yield return null;
