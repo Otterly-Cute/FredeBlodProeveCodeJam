bb84abb36f7d3878ea2afa28fdcc5abfadd0d63b
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2024-06-03 21:08:27 +0200

    notes

---
+++ Assets/Scripts/IkkeBrugt/Delegates.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Delegates : MonoBehaviour
+{
+
+    //lampda funktion er en funktion uden et navn = delegates?
+
+    public delegate void ThingyDoer(int item);
+
+    public static void DoStuff()
+    {
+        var list = new List<int> { 1,2,3};
+       
+        //tager input data og laver det om til noget andet
+        list.Select(x => x + 1);//Select gør at den gør det ved hver item i datasættet, den funktion der står i parentes er en lampda funktion     det bliver lavet til en ny liste
+        list.Where(x => x > 1); //Where vælger alle dem hvor at statement er true
+
+        DoStuffWithItems(list, x => Console.WriteLine(x));
+    }
+
+
+    static void DoStuffWithItems(List<int> items, Action<int> doer)
+    {
+        foreach (var item in items)
+        {
+            doer.Invoke(item);
+        }
+    }
+
+
+    //Action = den retunere ikke noget(void)
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/IkkeBrugt/Generics.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generics : MonoBehaviour
+{
+
+
+    //records,  den laver classen for en
+    public sealed record GenericClass<T>(T Item);
+
+    
+
+ 
+    void Start()
+    {
+        //generic kan tage flere forskellige slags variabler.
+        var x = new GenericClass<int>(Item: 1);
+        var y = new GenericClass<string>(Item: "hello");
+        var z = new GenericClass<int>(Item: 2);
+        //de kan ikke skifte type bagefter
+
+        GenericMethod(items: new List<int>() { 1, 2, 3, 4 });
+        GenericMethod(items: new List<string>() { "hello", "i'm", "Clara" });
+
+
+
+    }
+
+    //ved en generic method vi kan bruge forskellige typer variabler uden at skulle lave flere methods for hver slags (fx. en for strings og en for ints)
+    public static void GenericMethod<T>(List<T> items)
+    {
+        foreach (var item in items) 
+            {
+                Console.WriteLine($"Item: {item}");
+            }
+    }
+
+
+    
+
+
+   
+    void Update()
+    {
+        
+    }
+}
